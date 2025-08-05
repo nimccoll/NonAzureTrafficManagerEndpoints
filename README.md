e36c04d5beb7fa8c32449ea8ecd4e4dc91ab4eca
Nicholas McCollum <nimccoll@microsoft.com>
2025-08-05 11:39:51 -0400

    Initial check-in

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # NonAzureTrafficManagerEndpoints
 Sample PowerShell script to find all Traffic Manager Endpoints whose type is not "azureEndpoints"
+
+The included PowerShell script, TrafficManagerNonAzureEndpoints.ps1, included in this repository can assist in identifying Traffic Manager Profiles that include endpoints of type externalEndpoints and nestedEndpoints. Traffic Manager Profiles with endpoints of this type are impacted by the DigiCert Issuance and Renewal Changes documented [here](https://techcommunity.microsoft.com/blog/appsonazureblog/important-changes-to-app-service-managed-certificates-is-your-certificate-affect/4435193). This script will help you identify resources impacted by scenario 2 in the blog post.
+
+To make sure you identify all impacted resources, the account that executes this script must have at least Reader access to all of the subscriptions in your tenant.
---
+++ TrafficManagerNonAzureEndpoints.ps1
@@ -0,0 +1,40 @@
+﻿#===============================================================================
+# Microsoft Azure Customer Advisory and Resiliency Engineering
+# Find all Traffic Manager Endpoints whose type is not "azureEndpoints"
+#===============================================================================
+# Copyright © Microsoft Corporation.  All rights reserved.
+# THIS CODE AND INFORMATION IS PROVIDED "AS IS" WITHOUT WARRANTY
+# OF ANY KIND, EITHER EXPRESSED OR IMPLIED, INCLUDING BUT NOT
+# LIMITED TO THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE.
+#===============================================================================
+Connect-AzAccount
+$endPointInfo = @()
+$subscriptions = Get-AzSubscription
+
+$subscriptions | ForEach-Object {
+    $subscriptionId = $_.SubscriptionId
+    $subscriptionName = $_.Name
+    Set-AzContext -Subscription $_.SubscriptionId
+
+    $profiles = Get-AzTrafficManagerProfile
+    
+    $profiles | ForEach-Object {
+        $endPoints = $_.Endpoints
+        $endPoints | ForEach-Object {
+            If ($_.Type -ne "azureEndpoints") {
+                
+                $endPointInfo += [PSCustomObject]@{
+                    SubscriptionId = $subscriptionId
+                    SubscriptionName = $subscriptionName
+                    ResourceGroupName = $_.ResourceGroupName
+                    ProfileName = $_.ProfileName
+                    EndpointName = $_.Name
+                    EndpointType = $_.Type
+                    Target = $_.Target
+                }
+            }
+        }
+    }
+}
+$endPointInfo | Format-Table
